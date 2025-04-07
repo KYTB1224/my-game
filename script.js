bae8338eeb9c60ca20007aa5b516c206f4d9fdc8
KYTB1224 <keiyatabe@gmail.com>
2025-04-07 18:34:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ window.addEventListener('load', () => {
     }
 });
 
-import { generateSHA256, extendHashTo100Chars, getElementEmoji, getSkillEmoji, showDiscoveryPopup } from './utils.js';
+import { generateSHA256, extendHashTo100Chars, getElementEmoji, getSkillEmoji, showDiscoveryPopup } from './js/utils.js';
 
 import { 
     player1Monster, 
